b9b38d48d89aeb29263f7eec771d9373ffc9a9f1
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-05 16:44:37 +0000

    DTSPO-440 Ammend webhook-template.tf

--- terraform/webhook-template.tf
+++ terraform/webhook-template.tf
@@ -1,8 +1,8 @@
-resource "azurerm_template_deployment" "github_management_template" {
+resource "azurerm_resource_group_template_deployment" "github_management_template" {
   name                = "${var.runbook_name}_webhook"
   resource_group_name = azurerm_resource_group.rg_github_membership.name
   deployment_mode     = "Incremental"
-  template_body       = <<DEPLOY
+  template_content    = <<TEMPLATE
   {
     "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
     "contentVersion": "1.0.0.0",
@@ -23,5 +23,5 @@ resource "azurerm_template_deployment" "github_management_template" {
         }
     ]
   }
-  DEPLOY
+  TEMPLATE
 }
