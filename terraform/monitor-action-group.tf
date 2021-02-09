885827280a53615728d12a91da4cd7c88c0e2587
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-09 10:59:29 +0000

    DTSPO-440 Ammended monitor-action-group.tf

--- terraform/monitor-action-group.tf
+++ terraform/monitor-action-group.tf
@@ -1,13 +1,13 @@
 resource "azurerm_monitor_action_group" "github-membership-action-group" {
   name                = "DisabledUsersAlertsAction"
   resource_group_name = azurerm_resource_group.rg_github_membership.name
-  short_name          = "p0action"
+  short_name          = "disabled_user"
 
   automation_runbook_receiver {
     name                    = "${var.runbook_name}_receiver"
-    automation_account_id   = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourcegroups/github-membership-management/providers/microsoft.automation/automationaccounts/aaa001"
+    automation_account_id   = azurerm_automation_account.github_membership_automation.id
     runbook_name            = var.runbook_name
-    webhook_resource_id     = "/subscriptions/1baf5470-1c3e-40d3-a6f7-74bfbce4b348/resourcegroups/github-membership-management/providers/microsoft.automation/automationaccounts/aaa001/webhooks/webhook_alert"
+    webhook_resource_id     = "${azurerm_automation_account.github_membership_automation.id}/webhooks/webhook_alert"
     is_global_runbook       = true
     service_uri             = local.webhook_uri
     use_common_alert_schema = true
