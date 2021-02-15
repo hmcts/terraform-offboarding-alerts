391bb51011787351906641801f5a46faa5fdf022
Matt Slater <matt.slater@hmcts.net>
2021-02-15 11:55:25 +0000

    DTSPO-440 - add sub id

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.14.4"
   backend "azurerm" {
-    # subscription_id      = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+    subscription_id      = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
     resource_group_name  = "core-infra-intsvc-rg"
     storage_account_name = "cftptlintsvc"
     container_name       = "tfstate"
