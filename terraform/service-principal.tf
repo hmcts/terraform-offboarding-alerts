6ba48271df6f199ba99b8532bba9c64db27bee88
Matt Slater <matt.slater@hmcts.net>
2021-02-19 09:29:26 +0000

    DTSPO-440 - remove github_management rerferences

--- terraform/service-principal.tf
+++ terraform/service-principal.tf
@@ -1,34 +1,34 @@
-resource "azuread_application" "automation_account" {
+resource "azuread_application" "main" {
   display_name = "${var.automation_account_name}-automation-account"
 }
 
-resource "azuread_application_certificate" "automation_account" {
-  application_object_id = azuread_application.automation_account.id
+resource "azuread_application_certificate" "main" {
+  application_object_id = azuread_application.main.id
   type                  = "AsymmetricX509Cert"
   encoding              = "hex"
-  value                 = azurerm_key_vault_certificate.automation_account.certificate_data
-  end_date              = azurerm_key_vault_certificate.automation_account.certificate_attribute[0].expires
+  value                 = azurerm_key_vault_certificate.main.certificate_data
+  end_date              = azurerm_key_vault_certificate.main.certificate_attribute[0].expires
 
 }
 
-resource "azuread_service_principal" "automation_account" {
-  application_id = azuread_application.automation_account.application_id
+resource "azuread_service_principal" "main" {
+  application_id = azuread_application.main.application_id
 
-  depends_on = [azuread_application_certificate.automation_account]
+  depends_on = [azuread_application_certificate.main]
 }
 
-resource "azuread_service_principal_certificate" "automation_account" {
-  service_principal_id = azuread_service_principal.automation_account.id
+resource "azuread_service_principal_certificate" "main" {
+  service_principal_id = azuread_service_principal.main.id
   type                 = "AsymmetricX509Cert"
   encoding             = "hex"
-  value                = azurerm_key_vault_certificate.automation_account.certificate_data
-  end_date             = azurerm_key_vault_certificate.automation_account.certificate_attribute[0].expires
+  value                = azurerm_key_vault_certificate.main.certificate_data
+  end_date             = azurerm_key_vault_certificate.main.certificate_attribute[0].expires
 }
 
-resource "azurerm_key_vault_access_policy" "automation_account" {
-  key_vault_id = data.azurerm_key_vault.cert_key_vault.id
+resource "azurerm_key_vault_access_policy" "main" {
+  key_vault_id = data.azurerm_key_vault.main.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azuread_service_principal.automation_account.object_id
+  object_id    = azuread_service_principal.main.object_id
 
   secret_permissions = [
     "get",
