0d3eace17fef39f411e22adadf81b9aa66d1a2ff
Matt Slater <matt.slater@hmcts.net>
2021-02-19 10:51:20 +0000

    DTSPO-440 - change secret name

--- terraform/local.tf
+++ terraform/local.tf
@@ -1,5 +1,5 @@
 locals {
-  cert_name               = "${var.automation_account_name}-automation-account"
+  cert_name               = "${var.automation_account_name}-automation-account-auth"
   webhook_name            = "${var.runbook_name}-webhook"
   webhook_uri_secret_name = "webhook-uri-${var.automation_account_name}"
 }
