fd0b8ed8809c314ab8aa4e5421c5dc2ea3dd51c5
Matt Slater <matt.slater@hmcts.net>
2021-02-19 09:38:12 +0000

    DTSPO-440 - tidy up

--- terraform/webhook-template.tf
+++ terraform/webhook-template.tf
@@ -1,49 +1,43 @@
+data "azurerm_key_vault_secret" "webhook_uri" {
+  name         = local.webhook_uri_secret_name
+  key_vault_id = data.azurerm_key_vault.main.id
 
-resource "null_resource" "generate_uri" {
-  triggers = {
-      always = timestamp()
-  }
+  depends_on = [null_resource.generate_webhook_uri]
+}
 
+resource "null_resource" "generate_webhook_uri" {
   provisioner "local-exec" {
-    command = "uri=$(az rest -m post --header 'Accept=application/json' -u 'https://management.azure.com/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.rg_github_membership.name}/providers/Microsoft.Automation/automationAccounts/${azurerm_automation_account.github_membership_automation.name}/webhooks/generateUri?api-version=2015-10-31' -o tsv) && printf \"%s\" $uri > webhookUri.txt"
+    command = <<COMMAND
+az login --service-principal --username $clientId --password $secret --tenant $tenantId
+uri_api='https://management.azure.com/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.main.name}/providers/Microsoft.Automation/automationAccounts/${azurerm_automation_account.main.name}/webhooks/generateUri?api-version=2015-10-31'
+uri=$(az rest -m post --header 'Accept=application/json' -u "$uri_api" -o tsv)
+az keyvault secret set --vault-name ${data.azurerm_key_vault.main.name} --name ${local.webhook_uri_secret_name} --value "$uri"
+
+  COMMAND
   }
 }
 
-locals {
-  webhook_name = "${var.runbook_name}-${random_string.random.result}"
-}
-
-
-resource "random_string" "random" {
-  length = 8
-  special = false
-  keepers = {
-    time = timestamp()
-  }
-}
-
-resource "azurerm_resource_group_template_deployment" "github_management_template" {
-  provider = azurerm.arm_template
-  name                = "${var.runbook_name}_${random_string.random.result}"
-  resource_group_name = azurerm_resource_group.rg_github_membership.name
+resource "azurerm_resource_group_template_deployment" "main" {
+  #provider            = azurerm.arm_template
+  name                = "${var.runbook_name}-webook"
+  resource_group_name = azurerm_resource_group.main.name
   deployment_mode     = "Incremental"
-  depends_on      = [azurerm_automation_runbook.github_membership_runbook,null_resource.generate_uri]
   template_content    = <<TEMPLATE
   {
     "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
     "contentVersion": "1.0.0.0",
     "resources": [
         {
-        "name": "${azurerm_automation_account.github_membership_automation.name}/${local.webhook_name}",
+        "name": "${azurerm_automation_account.main.name}/${local.webhook_name}",
         "type": "Microsoft.Automation/automationAccounts/webhooks",
         "apiVersion": "2015-10-31",
         "properties": {
             "isEnabled": true,
-            "uri": "${replace("=${null_resource.generate_uri.id}=${file("webhookUri.txt")}", "=${null_resource.generate_uri.id}=", "")}",
+            "uri": "${data.azurerm_key_vault_secret.webhook_uri.value}",
             "expiryTime": "2030-01-01T00:00:00.000+00:00",
             "parameters": {},
             "runbook": {
-            "name": "${azurerm_automation_runbook.github_membership_runbook.name}"
+            "name": "${azurerm_automation_runbook.main.name}"
             }
         }
         }
